247a54116a22537c7c5bdce0c0daac3676982d9b
sasha <elshurimem1337@gmail.com>
2024-10-09 01:08:22 +0300

    Initial commit

---
+++ src/main/java/education/infoprotection/CryptoAnalysisMachine.java
@@ -0,0 +1,4 @@
+package education.infoprotection;
+
+public class CryptoAnalysisMachine {
+}
---
+++ src/main/java/education/infoprotection/KeyMachine.java
@@ -0,0 +1,41 @@
+package education.infoprotection;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public class KeyMachine {
+
+    public Optional<HashMap<String, Integer>> getTableKey() {
+        Path filepath = Paths.get("table-key-trysemus.txt");
+        HashMap<String, Integer> keys = new HashMap<>();
+        try (Stream<String> line = Files.lines(filepath)) {
+            List<String> lines = line.toList();
+            if (lines.size() == 2) {
+                keys.put("row", Integer.parseInt(lines.get(0)));
+                keys.put("column", Integer.parseInt(lines.get(1)));
+                return Optional.of(keys);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return Optional.empty();
+    }
+
+    public Optional<String> getTextKey() {
+        Path filePath = Paths.get("key-trysemus.txt");
+        try {
+            if (Files.exists(filePath) && Files.size(filePath) > 0) {
+                return Optional.of(Files.readString(filePath).toUpperCase());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return Optional.empty();
+    }
+}
